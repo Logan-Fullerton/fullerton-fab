3ab816bbb147120ed1842b23b571d25e193c2220
unknown <fullertonlogan@gmail.com>
2024-04-07 20:51:21 -0700

    added images

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -10,6 +10,67 @@ function About() {
           projects and everything in between; Fullerton Fab has you covered.
         </p>
       </div>
+
+      <div
+        id="carouselExampleAutoplaying"
+        className="carousel slide"
+        data-bs-ride="carousel"
+      >
+        <div className="carousel-inner">
+          <div className="carousel-item active">
+            <img
+              src="src\assets\metal pics\image0.jpeg"
+              className="d-block w-100 h-75"
+              alt="..."
+            />
+          </div>
+          <div className="carousel-item">
+            <img
+              src="src\assets\metal pics\image1.jpeg"
+              className="d-block w-100 h-75"
+              alt="..."
+            />
+          </div>
+          <div className="carousel-item">
+            <img
+              src="src\assets\metal pics\image2.jpeg"
+              className="d-block w-100 h-75"
+              alt="..."
+            />
+          </div>
+        </div>
+        <button
+          className="carousel-control-prev"
+          type="button"
+          data-bs-target="#carouselExampleAutoplaying"
+          data-bs-slide="prev"
+        >
+          <span
+            className="carousel-control-prev-icon"
+            aria-hidden="true"
+          ></span>
+          <span className="visually-hidden">Previous</span>
+        </button>
+        <button
+          className="carousel-control-next"
+          type="button"
+          data-bs-target="#carouselExampleAutoplaying"
+          data-bs-slide="next"
+        >
+          <span
+            className="carousel-control-next-icon"
+            aria-hidden="true"
+          ></span>
+          <span className="visually-hidden">Next</span>
+        </button>
+      </div>
+      <div className="container text-center">
+        <h1>Contact Us!</h1>
+        <ul>
+          <li>Phone: 530-867-6581</li>
+          <li>Email: fullertonlogan@gmail.com </li>
+        </ul>
+      </div>
     </div>
   );
 }
